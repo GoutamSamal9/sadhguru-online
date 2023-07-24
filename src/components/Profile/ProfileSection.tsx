dd47785a6c8460dc1253a9ab3d08df0c1272dcb6
goutamsamal9 <gsamal101@gmail.com>
2023-07-24 20:12:35 +0530

    Profile page all done

--- src/components/Profile/ProfileSection.tsx
+++ src/components/Profile/ProfileSection.tsx
@@ -5,14 +5,14 @@ const ProfileSection = () => {
   return (
     <>
       <ContainerLayout>
-        <div className="flex justify-center pt-3">
+        <div className="flex justify-center pt-6">
           <img
             className="h-40 w-40 rounded-full border-4 shadow-md"
             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
             alt=""
           />
         </div>
-        <div className="lg:w-3/4 sm:w-full h-auto py-8 px-4 m-auto mt-4">
+        <div className="lg:w-3/4 sm:w-full h-auto p-6 m-auto lg:mt-4">
           <div className="lg:flex justify-between items-center flex-wrap sm:block">
             <div className="lg:w-[48%] sm:w-full mt-3">
               <InputField
@@ -20,7 +20,7 @@ const ProfileSection = () => {
                 type="text"
                 name="name"
                 id="name"
-                className="bg-[#f6f0e7]"
+                className="bg-[#e3ded5]"
                 placeholder="Sankaran pilae"
                 value={"Sankaran pilae"}
                 disabled
@@ -32,7 +32,7 @@ const ProfileSection = () => {
                 type="email"
                 name="email"
                 id="email"
-                className="bg-[#f6f0e7]"
+                className="bg-[#e3ded5]"
                 placeholder="you@example.com"
                 value={"spilae@example.com"}
                 disabled
@@ -44,7 +44,7 @@ const ProfileSection = () => {
                 type="text"
                 name="Company Name"
                 id="companyName"
-                className="bg-[#f6f0e7]"
+                className="bg-[#e3ded5]"
                 placeholder="Isha"
                 value={"Isha"}
                 disabled
@@ -56,10 +56,76 @@ const ProfileSection = () => {
                 type="text"
                 name="VAT Number"
                 id="VATNumber"
-                className="bg-[#f6f0e7]"
+                className="bg-[#e3ded5]"
                 placeholder="1234567890"
                 value={"1234567890"}
                 disabled
+              />
+            </div>
+            <div className="lg:w-[48%] sm:w-full mt-3">
+              <InputField
+                label="Phone"
+                type="number"
+                name="Phone"
+                id="Phone"
+                className="bg-[#e3ded5]"
+                placeholder="9876543210"
+                value={"9876543210"}
+              />
+            </div>
+            <div className="lg:w-[48%] sm:w-full mt-3">
+              <InputField
+                label="Street"
+                type="text"
+                name="Street"
+                id="Street"
+                className="bg-[#e3ded5]"
+                placeholder="IYC, Coimbatore, TN, India"
+                value={"IYC, Coimbatore, TN, India"}
+              />
+            </div>
+            <div className="lg:w-[48%] sm:w-full mt-3">
+              <InputField
+                label="City"
+                type="text"
+                name="City"
+                id="city"
+                className="bg-[#e3ded5]"
+                placeholder="Coimbatore"
+                value={"Coimbatore"}
+              />
+            </div>
+            <div className="lg:w-[48%] sm:w-full mt-3">
+              <InputField
+                label="Zip / Postal Code"
+                type="text"
+                name="Zip / Postal Code"
+                id="zipCode"
+                className="bg-[#e3ded5]"
+                placeholder="754010"
+                value={"754010"}
+              />
+            </div>
+            <div className="lg:w-[48%] sm:w-full mt-3">
+              <InputField
+                label="Country"
+                type="text"
+                name="Country"
+                id="country"
+                className="bg-[#e3ded5]"
+                placeholder="Bharat"
+                value={"Bharat"}
+              />
+            </div>
+            <div className="lg:w-[48%] sm:w-full mt-3">
+              <InputField
+                label="State / Province"
+                type="text"
+                name="State / Province"
+                id="state"
+                className="bg-[#e3ded5]"
+                placeholder="Tamil Nadu"
+                value={"Tamil Nadu"}
               />
             </div>
           </div>
--- src/components/ReUsable/ContainerLayout/ContainerLayout.tsx
+++ src/components/ReUsable/ContainerLayout/ContainerLayout.tsx
@@ -2,12 +2,13 @@ import React, { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  className?: string
 }
 
-export default function ContainerLayout({ children }: Props) {
+export default function ContainerLayout({ children, className }: Props) {
   return (
     <div className="min-h-screen">
-      <div className="container mx-auto h-auto p-2 my-6 rounded-md">
+      <div className={`container mx-auto h-auto p-2 my-6 rounded-md ${className}`}>
         {children}
       </div>
     </div>
